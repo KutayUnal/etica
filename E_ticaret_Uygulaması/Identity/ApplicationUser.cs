1ba69e998c152a3d9123457668cd7febfaa4b3e0
KutayUnal <119403349+KutayUnal@users.noreply.github.com>
2023-07-15 13:58:46 +0300

    etica
    etica

---
+++ E_ticaret_Uygulaması/Identity/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace E_ticaret_Uygulaması.Identity
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
---
+++ E_ticaret_Uygulaması/Models/Login.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace E_ticaret_Uygulaması.Models
+{
+    public class Login
+    {
+        [Required]
+        [DisplayName("Kullanıcı Adı")]
+        public string UsurName { get; set; }
+        
+        [Required]
+        [DisplayName("Şifre")]
+        public string Password { get; set; }
+
+        [DisplayName("Beni Hatırla")]
+        public bool RememberMe { get; set; }
+    }
+}
